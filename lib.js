133198f915041814b55736eea013011f3297c37b
dienebsouza <dihcontatos@gmail.com>
2024-11-18 11:13:42 -0300

    Initial commit

---
+++ lib.js
@@ -0,0 +1,27 @@
+/*  console.log - para devs, só aparece no console
+    document.write - escreve no navegador mesmo, pode deixar os dois
+*/
+//console.log('O nome digitado foi', nm);
+/*var nm = prompt('Digite o nome');
+document.write('O nome digitado foi ', nm, '.');*/
+
+/*  var = variavel
+    parseInt = trasforma o texto em número
+*/
+var n1 = parseFloat(prompt('Digite um número'));
+var n2 = parseFloat(prompt('Digite outro número'));
+document.write('Resultado da soma: ', n1+n2 + "<br>");
+console.log('Resultado da soma: ', n1+n2);
+
+document.write('Resultado da subtração: ', n1-n2 + "<br>");
+console.log('Resultado da subtração: ', n1-n2);
+
+document.write('Resultado da multiplicação: ', n1*n2 + "<br>");
+console.log('A multiplicação foi ', n1*n2);
+
+document.write('Resultado da divisão: ', n1/n2 + "<br>");
+console.log('Resultado da divisão: ', n1/n2);
+
+
+
+
